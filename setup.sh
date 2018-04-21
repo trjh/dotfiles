2557010aa7d2b7110eb8a8214742bede93234017
Tim Hunter <huntert@gmail.com>
2018-04-21 18:04:19 +0100

    script to install the dot files as home dir files, linked to the repository copy

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# put these files into place
+cd $HOME
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.vimrc .
+ln -sf dotfiles/.gitconfig .
+ln -sf dotfiles/.colordiffrc .
+ln -sf dotfiles/.zshrc .
+ln -sf dotfiles/.zshenv .
+ln -sf setup/gitcheck.sh
